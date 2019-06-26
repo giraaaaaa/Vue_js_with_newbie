3e94d3e90741ea9e0ac9a1650a1e6340066df820
landamaizer <landamaizer@gmail.com>
2019-06-26 16:06:57 +0900

    ff

--- frontend/src/components/basic/Calculator.vue
+++ frontend/src/components/basic/Calculator.vue
@@ -1,142 +1,106 @@
 <template>
-       <div id="calculator">
-           <div class="result"><p></p></div>
-           <div @click="clear" class="clear">C</div>
-
-           <span @click="append('7')">7</span>
-           <span @click="append('8')">8</span>
-           <span @click="append('9')">9</span>
-           <span @click="add">+</span>
-
-           <span @click="append('4')">4</span>
-           <span @click="append('5')">5</span>
-           <span @click="append('6')">6</span>
-           <span>-</span>
-
-           <span @click="append('1')">1</span>
-           <span @click="append('2')">2</span>
-           <span @click="append('3')">3</span>
-           <span>/</span>
-
-           <span>0</span>
-           <span>.</span>
-           <span @click="equal">=</span>
-           <span>*</span>
-       </div>
-       
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="times" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
+  </div>
 </template>
 
 <script>
 export default {
-//    name:"Calculator"
-   data(){
-       return{
-            previous: null,
-            current: '',
-            operator: null,
-            operatorClicked: false
-       }       
-   },
-   methods: {
-       clear(){
-           this.current = '';
-       },
-       sign(){
-           this.current = this.current.charAt(0) === '-'?
-           this.current.slice(1) : '-${this.current}';
-       },
-       percent(){
-           this.current = '${parseFloat(this.current) / 100}';
-       },
-       dot(){
-           if(this.current/indexOf('.') === -1){
-               this.append('.');
-           }
-       },
-       setPrevious(){
-           this.previous = this.current;
-           this.operatorClicked = true;
-       },
-       add(){
-           this.operator = (a, b)=> a + b;
-           this.setPrevious
-       },
-       minus(){},
-       times(){},
-       divide(){},
-       append(number){
-           if(this.operatorClicked){
-               this.current = '';
-               this.operatorClicked = false;
-           }
-           this.current = '${this.current}${number}';
-       },
-       equal(){
-           this.current = '${this.operator{\
-           parseFloat(this.current),parseFloat(this.previous) \
-           }}';
-           this.previous = null;  
-       }
-   }
+  data() {
+    return {
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
+    
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+   
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous), 
+        parseFloat(this.current)
+      )}`;
+      console.log('결과 : '+this.current)
+      this.previous = null;
+    }
+  }
 }
 </script>
 
 <style scoped>
-
-
-#calculator {
- width: 450px;
- height: 270px;;
- margin: 50px auto 0 auto;
- background-color: #191970;
- padding: 10px 10px 260px 10px;
- border-radius: 5px;
- border-bottom: 5px solid #42729B;
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
 }
-
-.result {
- width: 305px;
- height: 40px;
- float: left;
- line-height: 40px;
- background-color: #fff;
- color: #000;
- margin: 5px;
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
-
-.result > p {
- margin-left: 10px;
+.zero {
+  grid-column: 1 / 3;
 }
-
-.clear, span {
- height: 40px;
- border-bottom: 3px solid grey;
- border-radius: 10px;
- text-align: center;
- line-height: 40px;
- cursor: pointer;
- background-color: #ecf0f1;
- color: #2c3e50;
- margin: 5px;
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
 }
-
-.clear {
- width: 95px;
- float: left;
- background-color: #c0392b;
- color: #fff;
-   border-color: #A63125;
+.operator {
+  background-color: orange;
+  color: white;
 }
-
-
-
-span {
- float: left;
- width: 95px;
-}
-span:hover{
-
-    background-color:#00BFFF;
-}
-
 </style>
